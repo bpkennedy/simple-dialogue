07319bc1ebfa50030f5fee5a1f5e13b76babd6e0
Brian Kennedy <bpkennedy@gmail.com>
2021-10-17 20:52:22 -0500

    Add jsdocs and github pages

--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
+/**
+ * Simple Dialogue contains in-memory management of branching dialogue web games
+ */
 import { fastFilter, fastFind } from './util'
+
+/**
+ * @typedef {Object} Dialogue
+ * @property {number} id unique id of the dialogue
+ * @property {string} message display text of the npc dialogue
+ * @property {number[]} [choices] array of dialogueIds that will be presented as
+ * choice prompts with this dialogue
+ * @property {function} [pre] optional prerequisite function that must return true
+ * @property {number} [preId] optional dialogue Id to use instead if using
+ * prerequisite function and prerequisite is false
+ * @property {function} [post] optional function to run after the npc speaks the dialogue
+ */
+
+/**
+ * @typedef {Dialogue} DialogueWithPrompts
+ * @property {Dialogue[]} prompts a hydrated array of Dialogues from the choices array
+ */
 
 const dialogues = {}
 const states = {}
@@ -35,6 +55,24 @@ function considerDialogue(npc, dialogue) {
   states[npc].currentId = nextDialogue.id
 }
 
+/**
+ * Interact with an npc will get the current line of dialogye or if choiceId
+ * argument is included will move the npc conversation to the next dialogue
+ * @method interactWith
+ * @param {string} npc The key to a specific npc
+ * @param {number} choiceId The dialogue id that the user speaks to the npc
+ * @returns {DialogueWithPrompts}
+ * @example
+ * // assumes es6 modules import style
+ * import { interactWith } from 'simple-dialogue'
+ *
+ * const johnnyResponse = interactWith('JohnnyBravo')
+ *
+ * // or to send specific dialogue choice to Johnny
+ *
+ * const johnnyResponse = interactWith('JohnnyBravo', 5)
+ *
+ */
 export const interactWith = (npc, choiceId) => {
   if (choiceId) {
     considerDialogue(npc, getDialogue(npc, choiceId))
@@ -42,11 +80,67 @@ export const interactWith = (npc, choiceId) => {
   return speakCurrentDialogue(npc)
 }
 
+/**
+ * Load Dialogues for a specific npc
+ * @method loadDialogue
+ * @param {string} npc The key to a specific npc
+ * @param {Dialogue[]} dialogueDataItems An array of the Dialogues
+ * that the npc will speak to the user
+ * @example
+ * // assumes es6 modules import style
+ * import { loadDialogue } from 'simple-dialogue'
+ *
+ * let johnnyRelationship = 60
+ * let playerTookQuest = false
+ * const takeQuest = () => playerTookQuest = true
+ * const dislikePlayer = () => johnnyRelationship -= 10
+ *
+ * const johnnyDialogues = [{
+ *    id: 1,
+ *    message: 'Would you like to take my quest?',
+ *    choices: [2, 3]
+ *  }, {
+ *    id: 2,
+ *    message: 'Sure, I\'ll take it.',
+ *    post: takeQuest,
+ *    next: 4
+ *  }, {
+ *    id: 3,
+ *    message: 'Nah, not worth my time.',
+ *    post: dislikePlayer,
+ *    next: 5
+ *  }]
+ *
+ * loadDialogue('JohnnyBravo', johnnyDialogues)
+ *
+ */
 export const loadDialogue = (npc, dialogueDataItems) => {
   dialogues[npc] = dialogueDataItems
   states[npc] = { currentId: dialogueDataItems[0].id }
 }
 
+/**
+ * Clear/delete Dialogues for a specific npc
+ * @method clearDialogue
+ * @param {string} npc The key to a specific npc
+ * @example
+ * // assumes es6 modules import style
+ * import { loadDialogue } from 'simple-dialogue'
+ *
+ * const johnnyDialogues = [{
+ *    id: 1,
+ *    message: 'Hello?',
+ *    choices: [2]
+ *  }, {
+ *    id: 2,
+ *    message: 'Nevermind.'
+ *  }]
+ *
+ * loadDialogue('JohnnyBravo', johnnyDialogues)
+ * // some time later....
+ * clearDialogue('JohnnyBravo')
+ *
+ */
 export const clearDialogue = (npc) => {
   if (dialogues[npc]) {
     dialogues[npc] = null
--- src/util.js
+++ src/util.js
@@ -1,12 +1,33 @@
-export const fastFind = (array, key, id) => {
+/**
+ * Utility functions for finding and iterating efficiently
+ */
+
+/**
+ * Find an item in an array of objects by key and value
+ * @ignore
+ * @function
+ * @param {string} array The collection to iterate
+ * @param {number} key The object property key to find
+ * @param {*} value The object property value to find
+ * @returns {(object|undefined)}
+ */
+export const fastFind = (array, key, value) => {
   for (let i = 0; i < array.length; i += 1) {
-    if (array[i][key] === id) {
+    if (array[i][key] === value) {
       return array[i]
     }
   }
   return undefined
 }
 
+/**
+ * Filter an array of objects by a filter function
+ * @ignore
+ * @function
+ * @param {string} array The collection to iterate
+ * @param {function} fn The function to filter by
+ * @returns {object[]}
+ */
 export const fastFilter = (array, fn) => {
   const final = []
   for (let i = 0; i < array.length; i += 1) {
